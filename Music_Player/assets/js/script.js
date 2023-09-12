9c8a333a6805bc1cc885a67dc279a92498521013
Momanyi Poet <momanyiturner@gmail.com>
2023-09-12 22:31:03 +0300

    assets

---
+++ Music_Player/assets/js/script.js
@@ -0,0 +1,8 @@
+// MULTIPLE TEXT
+let typed = new Typed('.multiple-text', {
+    strings: ["AFRO VIBE", "NO COPYRIGHT SONGS", "POP CHRONICLES", "AMAPIANO", "BONGO MAHABA"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 4000,
+    loop: true
+})
